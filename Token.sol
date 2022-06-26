bea321685fcc3cd1c4e3b82c97bbe3baae138973
Jonathan Lyon <jessicacrawfordoptimus010@gmail.com>
2022-06-26 15:00:15 -0400

    Second commit on Token.sol

--- Token.sol
+++ Token.sol
@@ -13,5 +13,20 @@ interface Token{
     //@return balance - The token of the owner
     function balanceOf(address _owner) external view returns (uint256 balance);
 
-    
+    /* @notice - Returns the remaining number of tokens that spender will be allowed to spend on behalf of owner through 
+    transferForm. This is zero by default. This value changes when approve or transferFrom are called.
+    @param _owner - The owner of the tokens
+    @param _spender - The authorized account to spend the balance of the owner.
+    @return remaining - The remaining tokens that can be spent
+     */
+    function allowance(address _owner, address _spender)external view returns (uint256 remaining);
+
+    /*@notice - Moves amount tokens from the caller's account to recipient
+    @param _to - The receiver of tokens 
+    @param _valuie - The value to be sent to the receiver
+    @return didOperationSucceed - Returns a boolean value indicating wether the operation succeeded
+     */
+     function transfer(address _to, uint256 _value) external returns (bool didOperationSucceed);
+
+     function  approve(address _spender, uint256 _value) external returns (bool didOperationSucceed);
 }
