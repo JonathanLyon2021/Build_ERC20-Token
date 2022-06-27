267f10a2148091f8b5807c4d1a29910ef5619f7a
Jonathan Lyon <jessicacrawfordoptimus010@gmail.com>
2022-06-26 22:35:29 -0400

    Final commit for BlockchainDevToken.sol

--- BlockchainDevToken.sol
+++ BlockchainDevToken.sol
@@ -19,4 +19,32 @@ contract BlockchainDevToken is StandardToken {
     uint256 public totalRaisedEthInWei;    ///EI is the smallest unit of ETH. Similar to cent is USD or Satoshi to Bitcoin. This is the TOTAL raised eth of the ICO.
     address payable public owner;     //The raised ETH will go to this account
 
+    constructor() {
+        decimals = 18;   // Number of decimals for your token.
+        _totalSupply = 1000000000000000000000;   //Update the total supply of your token. (1000 x 10^18)
+        _balances[msg.sender] = _totalSupply; //Grant the contract creator all initial tokens. In this case, it is set to 1000.
+                                                // Example: If you want your initial tokens to be 500 and your decimals is 6, set this value to: (500 * 10^6).
+    name = "Blockchain Dev Token";  //Set name. For display purposes.
+    symbol = "BDT"; //Set symbol. For display purposes.
+    unitOneEthCanBuy = 10;     // Set the price of your token for the ICO.
+                                // In this case, if users pay 1 ETH, they will receive 10 BDT.
+    owner = payable(msg.sender); // Owner of this contract get the total ETH raised.
+    }
+
+    /**
+    *As of Solidity 0.6.0, if you want your contract to receive Ether, you have to implement a receive Ether function.
+    *using payable fallback functions for receiving Ether is not reccommended, since it would not fail on interface confusions.
+    */
+    receive() external payable {
+        totalRaisedEthInWei = totalRaisedEthInWei + msg.value;
+        uint256 amount =msg.value * unitOneEthCanBuy;
+        require(_balances[owner] >= amount);
+        _balances[owner] -= amount;
+        _balances[msg.sender] += amount;
+        emit Transfer(owner, msg.sender, amount);
+        owner.transfer(msg.value);
+
+    }
+
+
 }
